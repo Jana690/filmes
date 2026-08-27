8b526e1aaa66300d5c9cc6e69b638728bcaeb613
Duolingo <j9199677@gmail.com>
2026-08-26 22:09:23 -0300

    Add HTML layout and Materialize framework
    Added HTML structure and included Materialize CSS and JS.

--- lista.php
+++ lista.php
@@ -1,3 +1,19 @@
+<html>
+	    <head>
+			   <title>lista de filmes</title>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body class="row">
+	    <div class="col l12">
+			   <div class="col l4" "card-panel teal lighten-2"
+			   <div class="col l8">Direita</div>
+        </div>
+    </body>
+</html
+	
+
 <$php
 include("conexao.php");
 
